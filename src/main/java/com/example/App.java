e984fae99c50ba141ca8c1d4436b24339474cde4
kouehema <kankoe-ange-herman.koue-hemazro@etu.u-paris.fr>
2022-12-04 13:19:43 +0100

    Debut javaFx

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,30 @@
 package com.example;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root,500,500);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 }
