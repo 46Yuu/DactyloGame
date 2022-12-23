9d282701f2e610490521eab4851ebb3140d9240f
tranh <henri-bergson.tran@etu.u-paris.fr>
2022-12-23 14:56:46 +0100

    caret et couleur texte

--- src/main/java/com/example/controller/MainSceneController.java
+++ src/main/java/com/example/controller/MainSceneController.java
@@ -1,11 +1,15 @@
 package com.example.controller;
 
+import org.fxmisc.richtext.Caret;
 import org.fxmisc.richtext.InlineCssTextArea;
 import org.fxmisc.richtext.StyleClassedTextArea;
+import org.fxmisc.richtext.NavigationActions.SelectionPolicy;
 
 import com.example.modele.Modele;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -33,19 +37,27 @@ public class MainSceneController {
 
     @FXML
     void areaOnKeyTyped(KeyEvent event) {
-        //System.out.println("\nUne key typed:  " + event.getCharacter());  
-
+        //System.out.println("\nUne key typed:  " + event.getCharacter());
     }
 
     @FXML
     void windowOnKeyTyped(KeyEvent event) {
         System.out.println("Une key typed:  " + event.getCharacter());
-
+        int caretPos = ictaArea.getCaretPosition();
+        System.out.println(ictaArea.getText(caretPos, caretPos+1));
+        if(event.getCharacter().compareTo(ictaArea.getText(caretPos, caretPos+1)) == 0){
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: green;");
+        }
+        else{
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: red;");
+        }
+        ictaArea.moveTo(caretPos+1);
     }
 
     public void setModele(Modele modele) {
         this.modele = modele;
-        modele.addListener(l -> {staArea.replaceText(l.getBeginningText());ictaArea.replaceText(l.getBeginningText());});
+        
+        modele.addListener(l -> {staArea.replaceText(l.getBeginningText());ictaArea.replaceText(l.getBeginningText());ictaArea.start(SelectionPolicy.CLEAR);});
     }
     
 
@@ -53,7 +65,6 @@ public class MainSceneController {
     void btIcta(ActionEvent event) {
         //Met la couleur rouge au texte de la position 0 à la position 10
         ictaArea.setStyle(0, 10, "-fx-fill: red;");
-        
     }
 
     
--- src/main/java/com/example/modele/Modele.java
+++ src/main/java/com/example/modele/Modele.java
@@ -37,21 +37,7 @@ public class Modele {
     }
 
     public void initialize(){
-        beginningText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et" +
-        "hendrerit tellus, fringilla rhoncus mauris. Curabitur et odio et justo mattis placerat." +
-        "Quisque venenatis mi erat, ac sollicitudin quam congue ut. Interdum et malesuada fames" +
-        "ac ante ipsum primis in faucibus. Nam non mauris interdum, vestibulum mi vel, condimentum" +
-        "ipsum. Suspendisse vitae finibus justo. Sed imperdiet, nisl ac dictum dapibus, massa orci" +
-        "molestie risus, sit amet lacinia velit neque sit amet lorem. Aliquam id velit ac erat" +
-        "ultricies blandit et eget ante. Nullam mattis ultricies venenatis. Pellentesque blandit" +
-        "gravida sem, quis accumsan risus venenatis eu. \nNullam condimentum interdum erat, id mattis " +
-        "nunc porttitor et. Sed efficitur accumsan semper. Suspendisse rhoncus leo vel euismod varius. " +
-        "Curabitur aliquam feugiat massa, ut finibus tortor vestibulum vel. Donec vitae lorem vel massa " +
-        "feugiat sodales. Sed at felis id turpis efficitur feugiat. Duis erat ante, eleifend nec fermentum " +
-        "in, feugiat pharetra augue. Pellentesque habitant morbi tristique senectus et netus et malesuada " +
-        "fames ac turpis egestas. Nullam sapien justo, venenatis id facilisis eget, dapibus eu justo. Donec " +
-        "elit nibh, egestas eu ullamcorper sit amet, cursus ac ipsum. Vestibulum pretium tellus id elit tincidunt," + 
-        "at tincidunt purus suscipit.\n" + 
+        beginningText = "Lorem ipsum dolor sit amet.\n" + 
         
         "Praesent non sodales ex. Donec posuere vestibulum leo, vel scelerisque nibh commodo nec. Sed ultrices " + 
         "sagittis aliquam. Proin quam quam, tincidunt sit amet pulvinar id, suscipit ut lacus. Nunc eget sagittis " + 
