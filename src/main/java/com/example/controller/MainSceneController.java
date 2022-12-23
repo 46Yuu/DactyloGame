e5a10398313acf95b48e959d4993b88b625ac538
tranh <henri-bergson.tran@etu.u-paris.fr>
2022-12-23 17:26:13 +0100

    backspace , consume not letter

--- src/main/java/com/example/controller/MainSceneController.java
+++ src/main/java/com/example/controller/MainSceneController.java
@@ -39,24 +39,78 @@ public class MainSceneController {
     void areaOnKeyTyped(KeyEvent event) {
         //System.out.println("\nUne key typed:  " + event.getCharacter());
     }
-
     @FXML
-    void windowOnKeyTyped(KeyEvent event) {
-        System.out.println("Une key typed:  " + event.getCharacter());
+    void areaOnKeyPressed(KeyEvent event){
         int caretPos = ictaArea.getCaretPosition();
+        String charTyped = event.getText();
+        System.out.println("Une key typed:  " + charTyped);
         System.out.println(ictaArea.getText(caretPos, caretPos+1));
-        if(event.getCharacter().compareTo(ictaArea.getText(caretPos, caretPos+1)) == 0){
+        if(charTyped.compareTo(ictaArea.getText(caretPos, caretPos+1)) == 0){
             ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: green;");
+            ictaArea.moveTo(caretPos+1);
+        }
+        else if(event.getCode() == KeyCode.BACK_SPACE){
+            ictaArea.setStyle(caretPos-1, caretPos, "-fx-fill: black;");
+            ictaArea.moveTo(caretPos-1);
+        }
+        else if(!event.getCode().isLetterKey()){
+            event.consume();
+        }
+        else if(event.getCode() == KeyCode.SPACE){
+            //valider 
         }
         else{
             ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: red;");
+            ictaArea.moveTo(caretPos+1);
         }
-        ictaArea.moveTo(caretPos+1);
     }
+
+    @FXML
+    void windowOnKeyTyped(KeyEvent event) {
+        /*int caretPos = ictaArea.getCaretPosition();
+        String charTyped = event.getCharacter();
+        System.out.println("Une key typed:  " + charTyped);
+        System.out.println(ictaArea.getText(caretPos, caretPos+1));
+        if(charTyped.compareTo(ictaArea.getText(caretPos, caretPos+1)) == 0){
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: green;");
+            ictaArea.moveTo(caretPos+1);
+        }
+        else{
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: red;");
+            ictaArea.moveTo(caretPos+1);
+        }*/
+        
+    }
+
+    /*@FXML
+    void windowOnKeyPressed(KeyEvent event){
+        int caretPos = ictaArea.getCaretPosition();
+        String charTyped = event.getText();
+        System.out.println("Une key typed:  " + charTyped);
+        System.out.println(ictaArea.getText(caretPos, caretPos+1));
+        if(charTyped.compareTo(ictaArea.getText(caretPos, caretPos+1)) == 0){
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: green;");
+            ictaArea.moveTo(caretPos+1);
+        }
+        else if(event.getCode() == KeyCode.BACK_SPACE){
+            ictaArea.setStyle(caretPos-1, caretPos, "-fx-fill: black;");
+            ictaArea.moveTo(caretPos-1);
+        }
+        else if(event.getCode() == KeyCode.SHIFT){
+            System.out.println("shift");
+        }
+        else if(event.getCode() == KeyCode.SPACE){
+
+        }
+        else{
+            ictaArea.setStyle(caretPos, caretPos+1, "-fx-fill: red;");
+            ictaArea.moveTo(caretPos+1);
+        }
+    }*/
 
     public void setModele(Modele modele) {
         this.modele = modele;
-        
+        ictaArea.setEditable(false);
         modele.addListener(l -> {staArea.replaceText(l.getBeginningText());ictaArea.replaceText(l.getBeginningText());ictaArea.start(SelectionPolicy.CLEAR);});
     }
     
